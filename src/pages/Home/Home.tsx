b22ee31f90238465e515b559f96607534d26624b
Gabriel Sampaio Bicalho <gabrielsampaiobicalho@gmail.com>
2022-04-08 18:25:40 -0300

    create Loading

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 import Card from "../../components/Card/Card";
 import OpenBoosterButton from "../../components/OpenBoosterButton/OpenBoosterButton";
